7caa19ae689664a6f7ceec8e7941eb22820b52bc
Magno Junior <magnosith@icloud.com>
2021-09-22 02:21:35 -0400

    FistCommit
    Just Save first data!

---
+++ Instaphoto/Instaphoto/InstaphotoApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstaphotoApp.swift
+//  Instaphoto
+//
+//  Created by magnodev on 22/09/21.
+//
+
+import SwiftUI
+
+@main
+struct InstaphotoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
